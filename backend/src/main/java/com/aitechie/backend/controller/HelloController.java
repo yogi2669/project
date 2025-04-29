fcaf5ed31d7f0e13f2f34db29bbb3f76fbc3594e
yogi2669 <yogeshsrivatsava2003@gmail.com>
2025-04-29 22:11:52 +0530

    Update HelloController.java

--- backend/src/main/java/com/aitechie/backend/controller/HelloController.java
+++ backend/src/main/java/com/aitechie/backend/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 public class HelloController {
 
     // Apply CORS to a specific method
-    @CrossOrigin(origins = "http://127.0.0.1:3333")  // Allow frontend at this origin
+    @CrossOrigin(origins = "http://localhost:60403/")  // Allow frontend at this origin
     @GetMapping("/api/hello")
     public String hello() {
         return "Hello Yogesh,I love u - Backend!";
